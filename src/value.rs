a5bd071840067c1d85553beed19696934ae7f466
robo-monk <rrobomonk@gmail.com>
2022-09-29 11:55:11 +0300

    progress

--- src/value.rs
+++ src/value.rs
@@ -1,6 +1,12 @@
 use std::ops::{Add, Sub, Mul};
+use std::collections::HashSet;
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    thread,
+};
 
-#[derive(Debug, Clone)]
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
 enum Operation {
     None,
     Add,
@@ -8,7 +14,8 @@ enum Operation {
     Mul,
 }
 
-#[derive(Debug, Clone)]
+static VALUE_COUNTER: AtomicUsize = AtomicUsize::new(0);
+#[derive(Debug, Clone, PartialEq)]
 pub struct Value<T> {
     label: String,
     data: T,
@@ -81,10 +88,47 @@ impl Mul for Value<f64> {
 }
 
 impl Value<f64> { 
-    fn backward(&mut self) -> () {
+
+    // def backward(self):
+
+    //     # topological order all of the children in the graph
+    //     topo = []
+    //     visited = set()
+    //     def build_topo(v):
+    //         if v not in visited:
+    //             visited.add(v)
+    //             for child in v._prev:
+    //                 build_topo(child)
+    //             topo.append(v)
+    //     build_topo(self)
+
+    //     # go one variable at a time and apply the chain rule to get its gradient
+    //     self.grad = 1
+    //     for v in reversed(topo):
+    //         v._backward()
+
+    fn backward(self) -> () {
+        let mut _topological_map: Vec<Value<f64>> = Vec::new();
+        let mut _visited: HashSet<Value<f64>> = HashSet::new();
+
+        fn build_topological_map(v: Value<f64>, topo: Vec<Value<f64>>, visited: HashSet<Value<f64>>){
+            visited.contains(v);
+            // if visited
+            // if visited
+        }
+
+    }
+
+    fn _backward(self) -> () {
         match self.operation {
             Operation::None => {}
             Operation::Add => {
+                if self.children.0.is_some() {
+                    self.children.0.unwrap().grad +=  self.grad;
+                }
+                if self.children.1.is_some() {
+                    self.children.1.unwrap().grad +=  self.grad;
+                }
             }
             Operation::Sub => {}
             Operation::Mul => {}
@@ -98,6 +142,8 @@ impl Value<f64> {
     }
 
     pub fn new(data: f64) -> Self {
+       Value(VALUE_COUNTER.fetch_add(1, Ordering::SeqCst));
+
         Value {
             data,
             grad: 0.0,
